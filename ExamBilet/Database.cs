9f01f485c5f330be2e118111955d230eb68b8bb9
petya <mityushin_petya@mail.ru>
2022-04-11 22:22:04 +0300

    second commit

---
+++ ExamBilet/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamBilet
+{
+    class Database
+    {
+        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-PNAU4VT;Initial Catalog=PrepExam;Integrated Security=true");
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+        public static DataTable RunQuery()
+        {
+            Database database = new Database();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+            DataTable dataTable = new DataTable();
+            SqlCommand command = new SqlCommand("SELECT * FROM UserViewStudent", database.getConnection());
+            sqlDataAdapter.SelectCommand = command;
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
---
+++ ExamBilet/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamBilet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Database database = new Database();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dataTable = Database.RunQuery();
+            string s1 = dataTable.Rows[0][0].ToString();
+            string s2 = dataTable.Rows[0][1].ToString();
+        }
+    }
+}
